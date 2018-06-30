bbd191e55cca62d39b4d5fe2bd1ae8c1b0e1827b
qmaolong <qmaolong@co-villa.com>
2018-06-30 23:41:31 +0800

    add project

---
+++ src/main/kotlin/com/dream/cowriting/CoWritingApplication.kt
@@ -0,0 +1,11 @@
+package com.dream.cowriting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoWritingApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoWritingApplication>(*args)
+}
